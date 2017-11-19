f9a5a73e32c6f7220c46fd3c3aef61a6f46d3fae
itisbenjamin <itisbenjamin@protonmail.ch>
2017-11-18 16:32:35 -0800

    finish sms message part

--- SMSmessages/get.py
+++ SMSmessages/get.py
@@ -1,23 +1,28 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+from time import sleep
+import requests
 
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope)
 gc = gspread.authorize(credentials)
-wks = gc.open("Data").sheet1
-values = wks.get_all_values()
-print(values)
-#data = dict(values)
-'''
-title = { 'Time', 'Sender Address', 'Sender Name', 'Message', 'First Attachment Name', 'First Attachment' }
 
-for item in values:
-    for i in range(6):
-        data[title[i]] = item[i]
-'''
-#print(data)
-#f = open("data.json", "w")
-#f.write(json.dumps(values))
-#print(values)
+title = ['time', 'name', 'text', 'url']
 
+while (True):
+    try:
+        wks = gc.open("Data").sheet1
+    except:
+        break
+    values = wks.get_all_values()
+    data = dict()
+    for item in values:
+        if not item[0]:
+            continue
+        for i in range(4):
+            data[title[i]] = item[i]
+        print(data)
+        requests.post('http://10.142.189.43:8080/twitterfeed', data=data)
+        sleep(5)
+    sleep(10)
