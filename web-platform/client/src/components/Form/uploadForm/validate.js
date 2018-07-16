e8ced63826fa76df6ee533f1f84ffa38933a98f8
alexter42 <lexie@protonmail.ch>
2018-07-16 00:16:27 -0400

    Upload new file to s3

--- web-platform/client/src/components/Form/uploadForm/validate.js
+++ web-platform/client/src/components/Form/uploadForm/validate.js
@@ -1,3 +1,43 @@
+import * as AWS from 'aws-sdk';
+
+const bucketName = process.env.DOCUMENTS_BUCKET_NAME;
+const bucketRegion = process.env.IDENTITY_POOL_REGION;
+const IdentityPoolId = process.env.IDENTITY_POOL_ID;
+
+const addFile = file => {
+  if (!file) {
+    return alert('Please choose a file to upload first.');
+  }
+  AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId,
+    }),
+  });
+
+  const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: { Bucket: bucketName },
+  });
+  const fileName = file.name;
+  const filesRocordKey = `${encodeURIComponent('docs')}/`;
+  const fileKey = filesRocordKey + fileName;
+  s3.upload(
+    {
+      Key: fileKey,
+      Body: file,
+      ACL: 'public-read',
+    },
+    (err, data) => {
+      if (err) {
+        return alert('There was an error uploading your photo: ', err.message);
+      }
+      alert('Successfully uploaded photo.');
+    }
+  );
+  return file;
+};
+
 export const createRecord = (form, data) => {
   const date = new Date();
 
