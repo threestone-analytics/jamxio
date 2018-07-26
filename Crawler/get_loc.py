5072798c6f728b1c585b9498c312e666a9cc84fe
itisbenjamin <itisbenjamin@protonmail.ch>
2018-07-25 18:25:21 -0700

    Twitter:  * add clean twitter geojson file

--- Crawler/get_loc.py
+++ Crawler/get_loc.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 def loop (keys, words_list):
     for key in keys:
@@ -6,45 +7,67 @@ def loop (keys, words_list):
             reutrn key
     return None
 
-def find_loc (article, states, cities, towns):
+def check_loc (cities, municipios, states):
+    if not states:
+        return None
+    if len(loc_starts_city) == 1:
+        return loc_starts_city[0]
+    if len(loc_starts_city) == 0:
+
+
+def find_loc (article, location_list):
     title = article['title'].split()
     summary = article['summary'].split()
     keywords = article['keywords']
     body = article['text'].split()
     words_list = title.append(summary.append(keywords.append(body)))
-    town = loop(towns, words_list)
-    if town:
-        return town
-    city = loop(cities, words_list)
-    if city:
-        return city
-    state = loop(states, words_list)
-    if state:
-        return state
-    return None
+    loc_starts_city = [ ]
+    loc_starts_municipio = [ ]
+    loc_starts_state = [ ]
+    for row in location_list:
+        state = row['NOM_ENT']
+        municipio = row['NOM_MUN']
+        city = row['NOM_LOC']
+        if city in words_list:
+            loc_starts_city.append(row)
+        elif municipio in words_list:
+            loc_starts_municipio.append(row)
+        elif state in words_list:
+            loc_starts_state.append(row)
+        else:
+            pass
+    if not loc_starts_state:
+        return 
+    if len(loc_starts_city) == 1:
+        article['location'] = [loc_starts_city[0]['lat_dd'], loc_starts_city[0]['lon_dd']]
+        return
+    elif len(loc_starts_city) == 0:
+        if len(loc_starts_municipio) == 1:
+            article['location'] = [loc_starts_municipio[0]['lat_dd'], loc_starts_municipio[0]['lon_dd']]
+        elif len(loc_starts_municipio) == 0:
+            pass
+            loop state
+        else:
+            check state
+            if len(loc_starts_state) == 0
+    else:
+        for item in loc_starts_city:
+            check muni and state
+    return article
 
 
 if __name__ == "__main__":
-    f = open('newsfeed.json', 'r')
-    newsfeed = json.load(f)
+    f = open('key_word_es.json', 'r')
+    keywords = json.load(f)
     f.close()
-    f = open('states', 'r')
-    states = json.load(f)
+    f = open('mex_shape.csv', encoding='latin-1', mode='r')
+    location_list = csv.DictReader(f)
     f.close()
-    f = open('cities', 'r')
-    cities = json.load(f)
-    f.close()
-    f = open('towns', 'r')
-    towns = json.load(f)
-    f.close()
-    for news in newsfeed:
-        org = news['Organization']
-        f = open(org + '.json', 'r')
-        articles = json.load(f)
-        f.close()
-        for article in articles:
-            loc = find_loc(article, states, cities, towns)
-            article['location'] = loc
-        f = open(org + '.json', 'w')
-        json.dump(articles, f)
-        f.close()
+    for keyword in keywords:
+        word = keyword['Environmental Justice Keywords in Spanish']
+        with open(word + '.json', encoding='latin-1', mode='r+') as f:
+            articles = json.load(f)
+            for article in articles:
+                loc = find_loc(article, location_list)
+                article['location'] = loc
+            json.dump(articles, f)
