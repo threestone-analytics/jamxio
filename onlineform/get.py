4ee04602a40ae8b559c0a4583659a54cfc417f7c
itisbenjamin <itisbenjamin@protonmail.ch>
2018-08-07 10:28:03 -0700

    Online Form & Twitter:  * clean twitter unused files  * retrieved online form data into geojson

--- onlineform/get.py
+++ onlineform/get.py
@@ -1,41 +1,68 @@
-import gspread
+#import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 from time import sleep
-import sqlite3
+#import sqlite3
 import googlemaps
 from keys import *
+import csv
 
 # google sheet credential
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-gc = gspread.authorize(credentials)
+#scope = ['https://spreadsheets.google.com/feeds']
+#credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+#gc = gspread.authorize(credentials)
 
 # db setup
-conn = sqlite3.connect('../db.sqlite')
-c = conn.cursor()
+#conn = sqlite3.connect('../db.sqlite')
+#c = conn.cursor()
 
 # google map credential
 gmaps = googlemaps.Client(key=google_map_key)
 
 if __name__ == "__main__":
-    wks = gc.open("Environmental justice Report (Responses)").sheet1
-    records = wks.get_all_records()
-    for r in records:
+    #wks = gc.open("Environmental justice Report (Responses)").sheet1
+    #records = wks.get_all_records()
+    '''
+    f = open('reports.csv', encoding='latin-1', mode='r')
+    reader = csv.DictReader(f)
+    f.close()
+    records_list = [ ]
+    for r in reader:
         try:
             location = gmaps.geocode(r["Where is the issue? Provide street, municipality, and state (don't include your personal information)"])
             lat = location[0]['geometry']['location']['lat']
             lng = location[0]['geometry']['location']['lng']
-            geojson = {
-                "type" : "Feature",
-                "properties" : { "id" : 0 },
+            r['location'] = [lat, lng]
+            records_list.append(r)
+            print('record retrieve!')
+        except:
+            print('bad record')
+        '''
+    f = open('data.json', encoding='latin-1', mode='r')
+    records = json.load(f)
+    f.close()
+    records_list = [ ]
+    for r in records:
+        lat = r['location'][0]
+        lng = r['location'][1]
+        if lat < 33 and lat > 14 and lng > - 118 and lng < - 87:
+            records_list.append(r)
+    geojson = {
+        "type" : "FeatureCollection",
+        "features" : [
+            {
+                  "type" :
+                "Feature",
                 "geometry" : {
                     "type" : "Point",
-                    "coordinates" : [lat, lng],
-                    }
-                }
-            c.execute("insert into onlineform (data, geojson) values (?, ?)", [json.dumps(r), json.dumps(geojson)])
-        except:
-            c.execute("insert into onlineform (data) values (?)", [json.dumps(r)])
-        print(r)
-        conn.commit()
+                    "coordinates" : [r['location'][1], r['location'][0]],
+                    },
+                "properties" : r,
+                } for r in records_list
+            ]
+        }
+    #with open('data.json', encoding='latin-1', mode='w') as f:
+    #    json.dump(records_list, f)
+    with open('data.geojson', encoding='latin-1', mode='w') as f:
+        json.dump(geojson, f)
+        
--- twitter/post.py
+++
@@ -1,8 +0,0 @@
-from keys import *
-import tweepy
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-api.update_status('test data')
